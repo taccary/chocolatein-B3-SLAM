17aa64bb3021efe75eea0d518d37cea4d96c4d2f
Tiphaine Accary-Barbier <Tiphaine.Accary-Barbier@icof.fr>
2021-08-30 11:16:57 +0200

    MAJ BDD

--- scripts_SQL/chocolatein.sql
+++ scripts_SQL/chocolatein.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 19 août 2021 à 17:11
+-- Généré le :  lun. 30 août 2021 à 07:25
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -21,8 +21,35 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `chocolatein`
 --
-CREATE DATABASE IF NOT EXISTS `chocolatein` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `chocolatein`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `actualite`
+--
+
+DROP TABLE IF EXISTS `actualite`;
+CREATE TABLE IF NOT EXISTS `actualite` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` text NOT NULL,
+  `contenu` text NOT NULL,
+  `datepublication` date NOT NULL DEFAULT current_timestamp(),
+  `actif` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `actualite`
+--
+
+INSERT INTO `actualite` (`id`, `titre`, `contenu`, `datepublication`, `actif`) VALUES
+(1, 'actu 1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lijov manef bemer joesib ba ve lam, tadiquamimi hadacym, tyquecibim. Loe leb fi fyl li cedeji duvep mijel, lafa lore fo, quylam. Mipa roemitol mije secydijehe hofobe mejaluqu pebelifequi den demyr sy. Tihi lisad foehu diquililusi ne, jimy bana colydoti quihebila fejy.<br />\r\n<br />\r\nSemi quyh safi ve mylelalivi sa he ha moeco, quebi fimym, hali, dilice. Loeda mam diqui se lob celafel jecihup mamulevi dilala teleco baqu vin quedoed, dybe. Mila danibe dudomu bedetal lafelecu noe doepa de, que fed sem malus. Beb, loet pimyqu bu nyquem jim bimo bi bajim ve lil ca tomi, je. Ja jihymeb dod sip bo dovu rejel doe fibitedim la fuvid dijobel, le.<br />\r\n<br />\r\nBaloep bicapyda lo tefocuv napire fe linoj ma jehe pe fu quac, bahe, naque. Pabil vuli deh doediquebi, sosacuhe bi biqu, fodin, dib, dedypy mi coe vebo. Meb netola, tehoemoero quade, bel, melemoe lyh taf lired naleb boca. Bacacu fypijepevo sepib dapehoe mubirelip, tade vi mylafe dehy, tamudilev myroema, cem dade, ra fe. Fyfoelehi, fi latol deny bel bibele tutejoda be nibidipi ferenary miv jolet.<br />\r\n<br />\r\nLalec pyp mi lifebid, byj male maqu foe mipadaqu lojyl dym toridobep medehoeruci. Bunabo jolefoelofe tido, moeserolic, hilimiral lo, moequy, lahydi dinyn lamem feb. Mibe mehi daly ba lib, mometil, tamyp tecob doel vamoese quir cehalequ miva majad be. Simoso, di mi tyd quiv na vena miv vab nifi faje lemalubi mupodoedi hoele. Penid le, cubide maqu pol roc nef, fib se bac.', '2021-08-18', 1),
+(2, 'actu 2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mibeluj, cocimi lyd meny, me li bel does, bemu, filamadi qua, timaquiro jibique bi. Tirilej civof, sap, va sanaliv fahymequ boer jile pamet muloericuc ny meliqu mecel je. Fib moelubed mebysej renebu le, cequa foequequa, dili jefale bem moed jab voevolijal. Pu quevevu saca milut mef male, milal til lilo qua diveri duqu made rilydeham lab.<br />\r\n<br />\r\nLedita fyde fet fami vim veci cyb fi mavadi buveruj felyfile. La lynu beji pevebeju dav fid deladav, bopa bequelaha bu, quemu mylidybeda. Boeb pol lijeledyb dalimy lalama fa, babenav, biboefamem biquebasi dacy. Hebi lori cyloe bamadequaf ba pij lo, fa tore tupi. Je papeliv fimoeh, loefy soehepo fof bemepu haloe tivefedufi mib melalebe bahej, balam.', '2021-08-26', 1),
+(3, 'actu 3', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Reb lilu bahe pesaf nijal bamo mem race tyva quim rodecov bahale. Bic ledemiqui sadepa dunal, lodas malab, ra, voe fev hus, foelido beb bubeci baqua. Lefe mil, lo viquab fahufaleb bel foemub jil tacelebafi dyloesi moenip. Fefilel batidapum, quoeme beli quim mydicod fic, nadacacoeh bil davib berib nele bi.', '2021-08-09', 0),
+(6, 'test de publi', 'qsdsdqsd', '2021-08-04', 0),
+(7, 'test de publi', '<p>qsdsdqsd</p>\r\n', '2021-08-04', 1),
+(10, 'test html', '<p><strong>test</strong> en <a href=\"http://google.com\" target=\"_blank\">html</a></p>\r\n\r\n<table border=\"1\" cellpadding=\"1\" cellspacing=\"1\" style=\"width:100%\" summary=\"qsd\">\r\n	<caption>aeaze</caption>\r\n	<tbody>\r\n		<tr>\r\n			<td>sqdqsd</td>\r\n			<td>&nbsp;</td>\r\n		</tr>\r\n		<tr>\r\n			<td>&nbsp;</td>\r\n			<td>&nbsp;</td>\r\n		</tr>\r\n		<tr>\r\n			<td>&nbsp;</td>\r\n			<td>qsqs</td>\r\n		</tr>\r\n	</tbody>\r\n</table>\r\n\r\n<p>&nbsp;</p>\r\n', '2021-08-18', 1),
+(11, 'test new tab', '<p>qsdqsd</p>\r\n', '2021-08-24', 1);
 
 -- --------------------------------------------------------
 
@@ -32,7 +59,7 @@ USE `chocolatein`;
 
 DROP TABLE IF EXISTS `contact`;
 CREATE TABLE IF NOT EXISTS `contact` (
-  `id` timestamp NOT NULL DEFAULT current_timestamp(),
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `personne` text DEFAULT NULL,
   `statut` text DEFAULT NULL,
   `mail` text DEFAULT NULL,
@@ -40,8 +67,21 @@ CREATE TABLE IF NOT EXISTS `contact` (
   `ville` text DEFAULT NULL,
   `site` text DEFAULT NULL,
   `message` text DEFAULT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp(),
+  `traite` tinyint(1) NOT NULL DEFAULT 0,
+  `datetraitement` datetime DEFAULT NULL,
+  `commentaire` text DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `contact`
+--
+
+INSERT INTO `contact` (`id`, `personne`, `statut`, `mail`, `telephone`, `ville`, `site`, `message`, `date`, `traite`, `datetraitement`, `commentaire`) VALUES
+(2, 'test de contact3', 'École', 'accary.tiphaine@free.fr', '', 'Lyon', '', 'prise de contact', '2021-08-28 19:28:18', 1, '2021-08-28 00:00:00', '<p>test de commentraire</p>\r\n'),
+(4, 'test ', 'Collectivité locale', 'accary.tiphaine@free.fr', '', 'Lyon', '', 'prise de contact', '2021-08-28 22:53:12', 0, '0000-00-00 00:00:00', ''),
+(5, 'test zaeaz', 'Collectivité locale', 'accary.tiphaine@free.fr', '', 'Lyon', '', 'prise de contact', '2021-08-28 22:53:18', 0, '0000-00-00 00:00:00', '');
 
 -- --------------------------------------------------------
 
@@ -144,8 +184,7 @@ INSERT INTO `gamme` (`id`, `libelle`, `picto`) VALUES
 ('confiseries', 'Confiseries', 'candy-cane'),
 ('dragees', 'Dragées et cadeaux invités', 'shopping-bag'),
 ('idees_cadeaux', 'Idées cadeaux', 'gifts'),
-('produits_de_saison', 'Produits de saison', 'calendar-alt'),
-('test', 'un beau test', 'atom');
+('produits_de_saison', 'Produits de saison', 'calendar-alt');
 
 -- --------------------------------------------------------
 
@@ -161,6 +200,14 @@ CREATE TABLE IF NOT EXISTS `infolettre` (
   `dateconfirmation` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `infolettre`
+--
+
+INSERT INTO `infolettre` (`id`, `email`, `confirmation`, `dateconfirmation`) VALUES
+('2021-08-27 16:33:23', 'tab@test.com', 0, NULL),
+('2021-08-27 16:33:43', 'accary.tiphaine@free.fr', 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -206,13 +253,29 @@ INSERT INTO `produit` (`id`, `nom`, `description`, `packaging`, `urlimg`, `idgam
 ('gateau_de_bonbons', 'Gâteau de bonbons', 'Assemblage de bonbons en forme de gâteau fourni dans une boîte de luxe', 'tout en élégance', './vues/images/produits/confiseries/gateau-bonbons', 'confiseries'),
 ('grelons', 'Grêlons', 'Noisettes caramélisées, enrobées de chocolat au lait, puis légèrement saupoudrées de sucre glace.', '150 grammes ou vrac', './vues/images/produits/dragees/grelons', 'dragees'),
 ('herisson', 'Hérisson', 'Chocolat praliné blanc ou lait.', 'environ 25 grammes', './vues/images/produits/saison/herisson', 'produits_de_saison'),
-('hh', 'hh', 'hh', 'hh', './vues/images/produits/chocolats/', 'chocolats'),
 ('mendiants', 'Mendiants', 'Composition de différents types de mendiants', '200 grammes', './vues/images/produits/chocolats/mendiants', 'chocolats'),
 ('outils_chocolat_noir', 'Outils en chocolat noir', 'Excellent cadeau à offrir ou à s\'offrir pour toutes les occasions ! Anniversaire, Noël ou encore pour la Saint Valentin.', '5 outils', './vues/images/produits/idees_cadeaux/outils', 'idees_cadeaux'),
 ('plateau_traiteur', 'Plateau traiteur', 'Plateau traiteur à composer devant les présentoirs.', '25 ou 49 chocolats', './vues/images/produits/chocolats/plateau', 'chocolats'),
 ('pralines_roses', 'Pralines roses', 'Des amandes enveloppées de sucre cuit, une gourmandise dont il est difficile de se détacher.', '200 grammes', './vues/images/produits/saison/praline-rose', 'produits_de_saison'),
 ('rose_chocolat', 'Rose en chocolat', 'Réalisée en chocolat au lait, cette rose fondante est décorée à la main.', '80 grammes', './vues/images/produits/idees_cadeaux/rose', 'idees_cadeaux'),
 ('tablettes', 'Tablettes', 'Tablettes de 70 grammes', '70 grammes', './vues/images/produits/chocolats/tablettes', 'chocolats');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reponse_contact`
+--
+
+DROP TABLE IF EXISTS `reponse_contact`;
+CREATE TABLE IF NOT EXISTS `reponse_contact` (
+  `idreponse` int(11) NOT NULL AUTO_INCREMENT,
+  `idcontact` int(11) NOT NULL,
+  `objet` text NOT NULL,
+  `contenu` text NOT NULL,
+  `datereponse` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`idreponse`),
+  UNIQUE KEY `idcontact` (`idcontact`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -247,7 +310,7 @@ CREATE TABLE IF NOT EXISTS `userdaily` (
   `date` date NOT NULL,
   `ip` text NOT NULL,
   PRIMARY KEY (`IDUSERDAILY`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `userdaily`
@@ -258,7 +321,11 @@ INSERT INTO `userdaily` (`IDUSERDAILY`, `date`, `ip`) VALUES
 (9, '2021-05-12', '127.0.0.1'),
 (11, '2021-05-12', '192.168.1.34'),
 (12, '2021-08-18', '127.0.0.1'),
-(13, '2021-08-19', '127.0.0.1');
+(13, '2021-08-19', '127.0.0.1'),
+(14, '2021-08-23', '127.0.0.1'),
+(15, '2021-08-24', '127.0.0.1'),
+(16, '2021-08-27', '127.0.0.1'),
+(17, '2021-08-28', '127.0.0.1');
 
 -- --------------------------------------------------------
 
@@ -301,6 +368,12 @@ ALTER TABLE `details_produits`
 --
 ALTER TABLE `produit`
   ADD CONSTRAINT `fk_idgamme` FOREIGN KEY (`idgamme`) REFERENCES `gamme` (`id`);
+
+--
+-- Contraintes pour la table `reponse_contact`
+--
+ALTER TABLE `reponse_contact`
+  ADD CONSTRAINT `reponse_contact_ibfk_1` FOREIGN KEY (`idcontact`) REFERENCES `contact` (`id`);
 
 --
 -- Contraintes pour la table `utilisateurs`
